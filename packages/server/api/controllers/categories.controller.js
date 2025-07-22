881f0b91480d713be41c855930ce82dd2ea4bb75
Andrii Gorokhovskyi <andrii.gorokhovskyi@gmail.com>
2025-07-22 15:03:37 +0200

    wip

--- packages/server/api/controllers/categories.controller.js
+++ packages/server/api/controllers/categories.controller.js
@@ -4,7 +4,17 @@ Can be deleted as soon as the first real controller is added. */
 const knex = require('../../config/db');
 
 const getCategories = async () => {
-  return knex('categories');
+  try {
+    const categories = await knex('categories')
+      .select('categories.*')
+      .distinct('categories.id')
+      .join('topics', 'topics.category_id', '=', 'categories.id')
+      .join('apps', 'apps.topic_id', '=', 'topics.id')
+      .orderBy('categories.title');
+    return categories;
+  } catch (error) {
+    return error.message;
+  }
 };
 
 module.exports = {
