43007020ef5c553e72e77d18288f5ccfcf88a977
Andrii Gorokhovskyi <andrii.gorokhovskyi@gmail.com>
2025-08-25 14:43:50 +0200

    wip

---
+++ packages/client/src/lib/constants/filters.js
@@ -0,0 +1,26 @@
+export const PRICING_OPTIONS = [
+  { key: 'FREE', label: 'Free' },
+  { key: 'FREEMIUM', label: 'Freemium' },
+  { key: 'PAID', label: 'Paid' },
+  { key: 'SUB', label: 'Subscription' },
+  { key: 'ONE', label: 'One time' },
+  { key: 'TRIAL', label: 'Trial available' },
+];
+
+export const PLATFORMS_OPTIONS = [
+  { key: 'EXT', label: 'Browser extension' },
+  { key: 'IOS', label: 'iOS app available' },
+  { key: 'ANDROID', label: 'Android app available' },
+  { key: 'WIN', label: 'Windows app' },
+  { key: 'MAC', label: 'Mac app' },
+];
+
+export const SOCIAL_MEDIA = [
+  { key: 'TWITTER', label: 'Twitter' },
+  { key: 'DISCORD', label: 'Discord' },
+];
+
+export const OTHER = [
+  { key: 'OS', label: 'Is Open Source?' },
+  { key: 'AI', label: 'Is AI powered?' },
+];
