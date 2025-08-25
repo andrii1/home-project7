e0a373009c70adbf18e3c03b1defec80a9bc1ec4
Andrii Gorokhovskyi <andrii.gorokhovskyi@gmail.com>
2025-08-25 12:41:22 +0200

    wip

--- packages/client/src/utils/getPageMeta.js
+++ packages/client/src/utils/getPageMeta.js
@@ -1,39 +1,93 @@
 import { capitalizeFirstWord } from './capitalizeFirstWord';
 
+const formatTitles = (titles) => {
+  if (titles.length <= 3) {
+    return titles.join(', ');
+  }
+
+  // Show first 3 + "and more"
+  return `${titles.slice(0, 3).join(', ')} and more`;
+};
+
+const buildMeta = (filtered, input, matchField, matchBy) => {
+  const titles = filtered
+    .map((val) => {
+      return input.find((item) => item[matchField] === matchBy(val))?.title;
+    })
+    .filter(Boolean);
+
+  const shortTitle = formatTitles(titles);
+  const capitalizedShort = capitalizeFirstWord(shortTitle);
+
+  return {
+    pageMetaTitle: `${capitalizedShort} apps - Try Top Apps`,
+    pageMetaDescription: `${capitalizedShort} apps - reviews, how to use, tutorials, deals, promo codes, errors.`,
+    pageHeaderTitle: `${capitalizedShort} apps`,
+  };
+};
+
 export const getPageMeta = ({
   filteredCategories,
-  categories: inputCategories,
+  categories,
   filteredTags,
-  tags: inputTags,
-  searchParam: inputSearchParam,
+  tags,
+  filteredFeatures,
+  features,
+  filteredUserTypes,
+  userTypes,
+  filteredBusinessModels,
+  businessModels,
+  filteredUseCases,
+  useCases,
+  filteredIndustries,
+  industries,
+  searchParam,
 }) => {
-  if (filteredCategories.length > 0) {
-    const categoryTitle = inputCategories.find(
-      (category) => category.id === parseInt(filteredCategories, 10),
+  if (filteredCategories !== undefined && filteredCategories.length > 0) {
+    return buildMeta(filteredCategories, categories, 'id', (val) =>
+      parseInt(val, 10),
     );
-    const title = categoryTitle?.title || 'this category';
-    const capitalizedTitle = capitalizeFirstWord(title);
-
-    return {
-      pageMetaTitle: `${capitalizedTitle} apps - Try Top Apps`,
-      pageMetaDescription: `${capitalizedTitle} apps - reviews, how to use, tutorials, deals, promo codes, errors.`,
-      pageHeaderTitle: `${capitalizedTitle} apps`,
-    };
   }
 
-  if (filteredTags.length > 0) {
-    const tagTitle = inputTags.find((tag) => tag.slug === filteredTags);
-    const title = tagTitle?.title || 'this topic';
-    const capitalizedTitle = capitalizeFirstWord(title);
-    return {
-      pageMetaTitle: `${capitalizedTitle} apps - Try Top Apps`,
-      pageMetaDescription: `${capitalizedTitle} apps - reviews, how to use, tutorials, deals, promo codes, errors.`,
-      pageHeaderTitle: `${capitalizedTitle} apps`,
-    };
+  if (filteredTags !== undefined && filteredTags.length > 0) {
+    return buildMeta(filteredTags, tags, 'slug', (val) => val);
   }
 
-  if (inputSearchParam) {
-    const capitalizedSearch = capitalizeFirstWord(inputSearchParam);
+  if (filteredFeatures !== undefined && filteredFeatures.length > 0) {
+    return buildMeta(filteredFeatures, features, 'id', (val) =>
+      parseInt(val, 10),
+    );
+  }
+
+  if (filteredUserTypes !== undefined && filteredUserTypes.length > 0) {
+    return buildMeta(filteredUserTypes, userTypes, 'id', (val) =>
+      parseInt(val, 10),
+    );
+  }
+
+  if (
+    filteredBusinessModels !== undefined &&
+    filteredBusinessModels.length > 0
+  ) {
+    return buildMeta(filteredBusinessModels, businessModels, 'id', (val) =>
+      parseInt(val, 10),
+    );
+  }
+
+  if (filteredUseCases !== undefined && filteredUseCases.length > 0) {
+    return buildMeta(filteredUseCases, useCases, 'id', (val) =>
+      parseInt(val, 10),
+    );
+  }
+
+  if (filteredIndustries !== undefined && filteredIndustries.length > 0) {
+    return buildMeta(filteredIndustries, industries, 'id', (val) =>
+      parseInt(val, 10),
+    );
+  }
+
+  if (searchParam) {
+    const capitalizedSearch = capitalizeFirstWord(searchParam);
     return {
       pageMetaTitle: `${capitalizedSearch} apps - Try Top Apps`,
       pageMetaDescription: `${capitalizedSearch} apps - reviews, how to use, tutorials, deals, promo codes, errors.`,
